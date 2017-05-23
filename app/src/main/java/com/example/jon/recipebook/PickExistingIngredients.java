e2798fc0a0442d7fa3c0477021e2880db3010e59
Jonathan Koser <jonathan.koser@gmail.com>
2017-05-22 21:05:16 -0500

    Fine, let's make an app
    I’m going to try and make an app. This will be fun…

---
+++ app/src/main/java/com/example/jon/recipebook/PickExistingIngredients.java
@@ -0,0 +1,13 @@
+package com.example.jon.recipebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickExistingIngredients extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_existing_ingredients);
+    }
+}
